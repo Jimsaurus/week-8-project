32784b47347abef30d28a42b4504ec80677dfc11
James McDermid <hello@jamesmcdermid.ca>
2015-08-20 19:33:42 -0400

    added bourbon refill

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,10 @@ var app = {};
 // INIT FUNCTION
 // =============================================================================
 app.init = function(){
+	$('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart',function (e) {
+	    $('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
+	    e.preventDefault();
+	  });
 	
 }; // end init function
 
